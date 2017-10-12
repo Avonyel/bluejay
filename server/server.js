996b752d9869035a189a0ccb475413d8794dc4f8
Stephanie Barker <song_mage@yahoo.com>
2017-10-12 13:14:06 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+// npm modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const localStrategy = require("passport-local").Strategy;
+
+// connect to database
+require("../mongoose/connect")();
+
+// server configurations
+const configs = require("./config");
+const mw = require("./middleware");
+
+// middleware
+app.use(session(configs.session));
+app.use(bodyParser.json());
+app.use(express.static("build"));
+app.use(mw.mongooseConnect);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/api", mw.authCheck);
+app.use(mw.socket(io));
+
+// passport setup
+passport.serializeUser(configs.serialize);
+passport.deserializeUser(configs.deserialize);
+passport.use(new localStrategy(require("../strategies/local")));
+
+// serve static resource
+app.get("/", (req, res) =>
+	res.sendFile(path.join(__dirname, "/build/index.html"))
+);
+
+// registering route
+app.use("/register", require("../routers/register"));
+
+// session handling routes
+app.use("/sessions", require("../routers/sessions"));
+
+// api routes
+app.use("/api/:resource", require("../routers")(io));
+
+// web sockets
+io.on("connection", require("./sockets")(io));
+
+// start server
+process.env.NODE_ENV === "production"
+	? server.listen(configs.port, configs.serverCallback)
+	: server.listen(configs.port, configs.host, configs.serverCallback);
---
+++ server/sockets.js
@@ -0,0 +1,19 @@
+const { User } = require("../models");
+const Events = require("../../client/src/redux/actions/events");
+
+module.exports = io => client => {
+	client.on(Events.USER_LOGGED_IN, async data => {
+		await User.findByIdAndUpdate(
+			data,
+			{ $set: { socketId: client.id } },
+			{ new: true }
+		);
+		io.to(client.id).emit(Events.REFRESH_NOTIFICATIONS);
+	});
+
+	// client.on("SEND_NOTIFICATION", async data => {
+	// 	console.log("Notification received...");
+	// 	const student = await User.findById(data);
+	// 	io.to(student.socketId).emit(Events.REFRESH_NOTIFICATIONS);
+	// });
+};
